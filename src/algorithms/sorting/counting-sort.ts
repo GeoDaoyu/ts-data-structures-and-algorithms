720ba6dfed5cfd6e285770f5a7af800b5bd17b60
zhanggy <zhanggy@geoscene.cn>
2023-06-06 18:10:21 +0800

    fix: counting-sort

--- src/algorithms/sorting/counting-sort.ts
+++ src/algorithms/sorting/counting-sort.ts
@@ -1,13 +1,11 @@
 const unsorted = [4, 3, 2, 6, 3, 8, 5, 7];
 const countingSort = (array: number[]): number[] => {
   const length = 10; // 0 - 9的排序
-  const bucket = new Array(length).fill(0);
+  const buckets: number[][] = new Array(length).fill(0).map(() => []);
   array.forEach((v) => {
-    bucket[v] = bucket[v] + 1;
+    buckets[v].push(v);
   });
-  return bucket.reduce((acc, curr, index) => {
-    return acc.concat(new Array(curr).fill(index));
-  }, []);
+  return buckets.flat();
 };
 
 console.log(unsorted);
